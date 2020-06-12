af8d58b3e0a2f6038f8026f1ef76e40ea30afde0
hari29harran <63388630+hari29harran@users.noreply.github.com>
2020-06-12 12:14:25 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             <div class="form-group">
                 <!-- Username Icon -->
                 <div class="img">
-                <?xml version="1.0" encoding="iso-8859-1"?>
+                
                 <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                     viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
                 <g>
@@ -82,7 +82,7 @@
             <div class="form-group">
                 <!-- Password Icon -->
                 <div class="img">
-                    <?xml version="1.0" encoding="iso-8859-1"?>
+                    
                     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                         viewBox="0 0 511.962 511.962" style="enable-background:new 0 0 511.962 511.962;" xml:space="preserve">
                     <g>
