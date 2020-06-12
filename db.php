ee2c70240353491e23087c8a6724e35063eee884
hari29harran <63388630+hari29harran@users.noreply.github.com>
2020-06-12 13:08:53 +0530

    Add files via upload

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-	$con=mysqli_connect("localhost","root","","guvi");
+	$con=mysqli_connect("sql12.freemysqlhosting.net","sql12347757","rGDJCVHUmG","sql12347757");
 	if(!$con)
 	{
 		echo mysqli_error($con);
